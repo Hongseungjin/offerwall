e24ef5e1b7cc13883fab632ca819742d9df03cad
abee <globalabee@gmail.com>
2023-07-07 20:20:06 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:flutter_overlay_window/flutter_overlay_window.dart';
-import 'package:sdk_eums/eum_app_offer_wall/eums_app_i.dart';
+import 'package:sdk_eums/common/local_store/local_store.dart';
+import 'package:sdk_eums/common/local_store/local_store_service.dart';
 import 'package:sdk_eums/eum_app_offer_wall/utils/appColor.dart';
 import 'package:sdk_eums/notification/true_caller_overlay.dart';
-import 'package:sdk_eums/sdk_eums.dart';
+import 'package:sdk_eums/sdk_eums_library.dart';
 
 final receivePort = ReceivePort();
 
@@ -22,6 +23,7 @@ void onStart(ServiceInstance service) {
     IsolateNameServer.removePortNameMapping('overlay_window');
   }
   receivePort.listen((message) async {
+    print("receivePort.asBroadcastStream()$message");
     if (message == 'openApp') {}
     if (message is Map) {
       try {
@@ -59,17 +61,18 @@ void main() {
   });
   runApp(const MaterialApp(home:  MyApp()));
 }
-
 @pragma("vm:entry-point")
 void overlayMain() {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
     statusBarColor: Colors.transparent,
   ));
-
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: TrueCallerOverlay(),
-  ));
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TrueCallerOverlay(),
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -79,7 +82,56 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp>  with WidgetsBindingObserver, SingleTickerProviderStateMixin {
+  LocalStore? localStore;
+  @override
+  void initState() {
+    localStore = LocalStoreService();
+    print("con cac");
+    super.initState();
+    checkOpen();
+    WidgetsBinding.instance.addObserver(this);
+    SdkEums.instant.permission.checkPermission();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    switch (state) {
+      case AppLifecycleState.resumed:
+        checkOpen();
+        break;
+      case AppLifecycleState.inactive:
+        break;
+      case AppLifecycleState.paused:
+        break;
+      case AppLifecycleState.detached:
+        break;
+    }
+  }
+
+
+
+  checkOpen() async {
+    print("vao day khong");
+    localStore = LocalStoreService();
+    Future.delayed(const Duration(milliseconds: 500), () async {
+      dynamic data = await localStore?.getDataShare();
+      print("dataslkdjalsjk;lda$data");
+      if (data == null || data == '') {
+      } else {
+        // ignore: use_build_context_synchronously
+        EumsAppOfferWallService.instance.openSdk(context,
+            memId: "abee997",
+            memGen: "w",
+            memBirth: "2000-01-01",
+            memRegion: "인천_서");
+        Future.delayed(const Duration(seconds: 2), (){
+          localStore!.removeKey('SAVE_DATA_SHARE');
+          print("vao day");
+        });
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
